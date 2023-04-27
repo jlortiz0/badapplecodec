37d05047b5d0de9dea841df720a9d29b5c58f914
jlortiz0 <jlortiz0@users.noreply.github.com>
2023-04-26 20:34:39 -0700

    Downgrade to 1bpp

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-var ccColors = []byte{0x19, 0x4C, 0x99, 0xF0}
-
 func NewImageEncoder(c <-chan []byte, h, w int, wr io.Writer, closed chan<- struct{}) {
 	buf := bufio.NewWriter(wr)
 	defer buf.Flush()
@@ -29,7 +27,7 @@ func NewImageEncoder(c <-chan []byte, h, w int, wr io.Writer, closed chan<- stru
 	defer close(c2)
 
 	e := NewDiffRLEEncoder()
-	b := make([]byte, h*w)
+	b := make([]byte, (h*w)/2)
 	lastFrame := make([]byte, len(b))
 
 	for {
@@ -37,25 +35,19 @@ func NewImageEncoder(c <-chan []byte, h, w int, wr io.Writer, closed chan<- stru
 		if !ok {
 			break
 		}
-		for i := 0; i < len(b); i++ {
-			bestInd := 0
-			bestDiff := byte(255)
-			for j, x := range ccColors {
-				diff := int16(b2[i*4]) - int16(x)
-				if diff < 0 {
-					diff = -diff
-				}
-				if byte(diff) < bestDiff {
-					bestDiff = byte(diff)
-					bestInd = j
-				}
+		for i := 0; i < len(b2); i += 8 {
+			var a byte
+			if b2[i] & 0x80 != 0 {
+				a += 2
 			}
-			b[i] = byte(bestInd)
+			if b2[i + 4] & 0x80 != 0 {
+				a += 1
+			}
+			b[i / 8] = a
 		}
-		split1, split2 := splitNibbles(b)
-		c1 <- WorkerEncodeThreadData{e: e.Copy(), data1: split1, data2: split2}
-		split1, split2 = splitNibbles(XORTheseArrays(b, lastFrame))
-		c2 <- WorkerEncodeThreadData{e: e.Copy(), data1: split1, data2: split2, diff: true}
+		c1 <- WorkerEncodeThreadData{e: e.Copy(), data: b}
+		diffed := XORTheseArrays(b, lastFrame)
+		c2 <- WorkerEncodeThreadData{e: e.Copy(), data: diffed, diff: true}
 		d1 := <-c1
 		d2 := <-c2
 		if d1.e.Len() < d2.e.Len() {
@@ -87,51 +79,38 @@ func NewImageDecoder(c chan<- []byte, rd io.Reader) {
 	c <- lastFrame
 	h := int(binary.BigEndian.Uint16(lastFrame))
 	w := int(binary.BigEndian.Uint16(lastFrame[2:]))
-	lastFrame = make([]byte, h*w)
+	lastFrame = make([]byte, (h*w)/2)
 	d := NewDiffRLEDecoder(buf)
-	b1 := make([]byte, (h*w)/2)
-	b2 := make([]byte, (h*w)/2)
+	b := make([]byte, (h*w)/2)
 
 	for {
-		header1, e := d.ReadHeader(2)
+		header, e := d.ReadHeader(2)
 		if !e {
 			break
 		}
-		for i := 0; i < len(b1); i++ {
-			b1[i], e = d.ReadCrumb()
+		for i := 0; i < len(b); i++ {
+			b[i], e = d.ReadCrumb()
 			if !e {
 				return
 			}
 		}
-		header2, e := d.ReadHeader(2)
-		if !e {
-			panic("unexpected EOF")
-		}
-		for i := 0; i < len(b2); i++ {
-			b2[i], e = d.ReadCrumb()
-			if !e {
-				panic("unexpected EOF")
-			}
-		}
-		if header2&HEADER_XOR != 0 {
-			b2 = XORTheseArrays(b2, b1)
-		}
-		if header1&HEADER_SWAP != 0 {
-			temp := b1
-			b1 = b2
-			b2 = temp
-		}
-		b := combineNibbles(b1, b2)
-		if header1&HEADER_DIFF != 0 {
+		if header&HEADER_DIFF != 0 {
 			b = XORTheseArrays(b, lastFrame)
 		}
-		lastFrame = b
 
-		temp := make([]byte, len(b))
+		temp := make([]byte, len(b) * 2)
 		for i, x := range b {
-			temp[i] = ccColors[x]
+			if x & 2 != 0 {
+				temp[2 * i] = 0xFF
+			}
+			if x & 1 != 0 {
+				temp[2 * i + 1] = 0xFF
+			}
 		}
 		c <- temp
+		temp = lastFrame
+		lastFrame = b
+		b = temp
 	}
 }
 
@@ -146,7 +125,7 @@ func main() {
 		main2()
 		return
 	}
-	if os.Args[1] != "encode" || len(os.Args) < 3 {
+	if os.Args[1] != "encode" || len(os.Args) < 4 {
 		fmt.Println("Usage:", os.Args[0], "encode <video> or", os.Args[0], "play <.bac file>")
 		return
 	}
@@ -289,51 +268,3 @@ func main2() {
 	}
 }
 
-func splitNibbles(b []byte) ([]byte, []byte) {
-	out1 := make([]byte, len(b)/2)
-	out2 := make([]byte, len(b)/2)
-	for i := 0; i < len(b)/2; i++ {
-		var b1, b2 byte
-		temp := b[2*i]
-		if temp&2 != 0 {
-			b1 |= 2
-		}
-		if temp&1 != 0 {
-			b2 |= 2
-		}
-		temp = b[2*i+1]
-		if temp&2 != 0 {
-			b1 |= 1
-		}
-		if temp&1 != 0 {
-			b2 |= 1
-		}
-		out1[i] = b1
-		out2[i] = b2
-	}
-	return out1, out2
-}
-
-func combineNibbles(b1, b2 []byte) []byte {
-	out := make([]byte, len(b1)+len(b2))
-	for i := 0; i < len(b1); i++ {
-		var d1, d2 byte
-		temp := b1[i]
-		if temp&2 != 0 {
-			d1 |= 2
-		}
-		if temp&1 != 0 {
-			d2 |= 2
-		}
-		temp = b2[i]
-		if temp&2 != 0 {
-			d1 |= 1
-		}
-		if temp&1 != 0 {
-			d2 |= 1
-		}
-		out[2*i] = d1
-		out[2*i+1] = d2
-	}
-	return out
-}
