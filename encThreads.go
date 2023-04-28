ca412c84bcd14afc5f428e86023a0497868232c2
jlortiz0 <jlortiz0@users.noreply.github.com>
2023-04-27 23:36:29 -0700

    Only use one bitplane

--- encThreads.go
+++ encThreads.go
@@ -15,77 +15,25 @@ func XORTheseArrays(data, rodata []byte) []byte {
 	return out
 }
 
-type WorkerWorkerEncodeThreadData struct {
+type WorkerEncodeThreadData struct {
 	e    *DiffRLEEncoder
 	data []byte
 	diff bool
 }
 
-func WorkerWorkerEncodeThread(c chan WorkerWorkerEncodeThreadData) {
+func WorkerEncodeThread(c chan WorkerEncodeThreadData) {
 	for {
 		d, ok := <-c
 		if !ok {
 			return
 		}
-		header := HEADER_SWAP
+		header := HEADER_NORMAL
 		if d.diff {
 			header |= HEADER_DIFF
 		}
 		d.e.BeginFrame(header, 2, d.data[0])
 		for _, x := range d.data[1:] {
 			d.e.WriteCrumb(x)
-		}
-		c <- d
-		d = <-c
-		d.e.BeginFrame(HEADER_DIFF, 2, d.data[0])
-		for _, x := range d.data[1:] {
-			d.e.WriteCrumb(x)
-		}
-		c <- d
-	}
-}
-
-type WorkerEncodeThreadData struct {
-	e     *DiffRLEEncoder
-	data1 []byte
-	data2 []byte
-	diff  bool
-}
-
-func WorkerEncodeThread(c chan WorkerEncodeThreadData) {
-	c2 := make(chan WorkerWorkerEncodeThreadData)
-	go WorkerWorkerEncodeThread(c2)
-	defer close(c2)
-	for {
-		d, ok := <-c
-		if !ok {
-			return
-		}
-		c2 <- WorkerWorkerEncodeThreadData{e: d.e.Copy(), data: d.data2, diff: d.diff}
-		header := HEADER_NORMAL
-		if d.diff {
-			header |= HEADER_DIFF
-		}
-		d.e.BeginFrame(header, 2, d.data1[0])
-		for _, x := range d.data1[1:] {
-			d.e.WriteCrumb(x)
-		}
-		d2 := <-c2
-		swap := d.e.Len() > d2.e.Len()
-		if swap {
-			d.e = d2.e
-			c2 <- WorkerWorkerEncodeThreadData{e: d.e.Copy(), data: d.data1}
-		} else {
-			c2 <- WorkerWorkerEncodeThreadData{e: d.e.Copy(), data: d.data2}
-		}
-		data := XORTheseArrays(d.data1, d.data2)
-		d.e.BeginFrame(HEADER_XOR|HEADER_DIFF, 2, data[0])
-		for _, x := range data[1:] {
-			d.e.WriteCrumb(x)
-		}
-		d2 = <-c2
-		if d.e.Len() > d2.e.Len() {
-			d.e = d2.e
 		}
 		c <- d
 	}
--- main.go
+++ main.go
@@ -52,10 +52,8 @@ func NewImageEncoder(c <-chan []byte, h, w int, wr io.Writer, closed chan<- stru
 			}
 			b[i] = byte(bestInd)
 		}
-		split1, split2 := splitNibbles(b)
-		c1 <- WorkerEncodeThreadData{e: e.Copy(), data1: split1, data2: split2}
-		split1, split2 = splitNibbles(XORTheseArrays(b, lastFrame))
-		c2 <- WorkerEncodeThreadData{e: e.Copy(), data1: split1, data2: split2, diff: true}
+		c1 <- WorkerEncodeThreadData{e: e.Copy(), data: b}
+		c2 <- WorkerEncodeThreadData{e: e.Copy(), data: XORTheseArrays(b, lastFrame), diff: true}
 		d1 := <-c1
 		d2 := <-c2
 		if d1.e.Len() < d2.e.Len() {
@@ -89,56 +87,36 @@ func NewImageDecoder(c chan<- []byte, rd io.Reader) {
 	w := int(binary.BigEndian.Uint16(lastFrame[2:]))
 	lastFrame = make([]byte, h*w)
 	d := NewDiffRLEDecoder(buf)
-	b1 := make([]byte, (h*w)/2)
-	b2 := make([]byte, (h*w)/2)
+	b := make([]byte, (h * w))
 
 	for {
-		header1, e := d.ReadHeader(2)
+		header, e := d.ReadHeader(2)
 		if !e {
 			break
 		}
-		for i := 0; i < len(b1); i++ {
-			b1[i], e = d.ReadCrumb()
+		for i := 0; i < len(b); i++ {
+			b[i], e = d.ReadCrumb()
 			if !e {
 				return
 			}
 		}
-		header2, e := d.ReadHeader(2)
-		if !e {
-			panic("unexpected EOF")
-		}
-		for i := 0; i < len(b2); i++ {
-			b2[i], e = d.ReadCrumb()
-			if !e {
-				panic("unexpected EOF")
-			}
-		}
-		if header2&HEADER_XOR != 0 {
-			b2 = XORTheseArrays(b2, b1)
-		}
-		if header1&HEADER_SWAP != 0 {
-			temp := b1
-			b1 = b2
-			b2 = temp
-		}
-		b := combineNibbles(b1, b2)
-		if header1&HEADER_DIFF != 0 {
+		if header&HEADER_DIFF != 0 {
 			b = XORTheseArrays(b, lastFrame)
 		}
-		lastFrame = b
 
 		temp := make([]byte, len(b))
 		for i, x := range b {
 			temp[i] = ccColors[x]
 		}
 		c <- temp
+		temp = lastFrame
+		lastFrame = b
+		b = temp
 	}
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		os.Args = append(os.Args, "play", "badapple.bac")
-		main2()
 		fmt.Println("Usage:", os.Args[0], "encode <video> <output> or", os.Args[0], "play <.bac file>")
 		return
 	}
@@ -287,53 +265,4 @@ func main2() {
 	for event.GetType() != sdl.KEYDOWN && event.GetType() != sdl.QUIT {
 		event = sdl.WaitEvent()
 	}
-}
-
-func splitNibbles(b []byte) ([]byte, []byte) {
-	out1 := make([]byte, len(b)/2)
-	out2 := make([]byte, len(b)/2)
-	for i := 0; i < len(b)/2; i++ {
-		var b1, b2 byte
-		temp := b[2*i]
-		if temp&2 != 0 {
-			b1 |= 2
-		}
-		if temp&1 != 0 {
-			b2 |= 2
-		}
-		temp = b[2*i+1]
-		if temp&2 != 0 {
-			b1 |= 1
-		}
-		if temp&1 != 0 {
-			b2 |= 1
-		}
-		out1[i] = b1
-		out2[i] = b2
-	}
-	return out1, out2
-}
-
-func combineNibbles(b1, b2 []byte) []byte {
-	out := make([]byte, len(b1)+len(b2))
-	for i := 0; i < len(b1); i++ {
-		var d1, d2 byte
-		temp := b1[i]
-		if temp&2 != 0 {
-			d1 |= 2
-		}
-		if temp&1 != 0 {
-			d2 |= 2
-		}
-		temp = b2[i]
-		if temp&2 != 0 {
-			d1 |= 1
-		}
-		if temp&1 != 0 {
-			d2 |= 1
-		}
-		out[2*i] = d1
-		out[2*i+1] = d2
-	}
-	return out
 }
